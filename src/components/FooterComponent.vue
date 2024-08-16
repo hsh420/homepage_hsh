9a82349568cfb8e862f8cee0733c4d6513fba7b6
Hendrik Schwind-Hansen <info@hendrik-sh.de>
2024-08-16 12:14:04 +0200

    implemented simple footer

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <footer>
+    <RouterLink :to="{ name: 'impressum' }">Impressum</RouterLink>
+    <RouterLink :to="{ name: 'datenschutz' }">Datenschutz</RouterLink>
+    <p>test test</p>
+  </footer>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,3 @@
-<script setup lang="ts"></script>
-
 <template>
   <main>
     <p>Hier kommt mal Inhalt hin!</p>
